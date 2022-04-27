dc71f968e23faaabe6ed497bd5612cde1cf58e81
Shanti Betts <Shanti.Betts@Gmail.com>
2022-04-27 16:35:45 -0700

    Asset reorganization and working weather API

--- main.js
+++ main.js
@@ -25,3 +25,27 @@ const unsplashAccessKey = 'H77Joy6-hev-qWlLnZ7Tw3URG_4lrjz5yhDXqdP3oPY'
 // 	.catch(() => {
 // 		console.log('error')
 // 	})
+
+$.ajax({
+	method: 'get',
+	url: `https://api.weather.gov/points/37.7,-119.6`
+})
+	.then((response) => {
+		console.log(response)
+		$.ajax({
+			method: 'get',
+			url: response.properties.forecastHourly
+		})
+			.then((response) => {
+				console.log(response)
+				$('.weather').append(
+					`<p>${response.properties.periods[0].temperature}ºF</p>`
+				)
+			})
+			.catch(() => {
+				console.log('error in 2nd pull')
+			})
+	})
+	.catch(() => {
+		console.log('error')
+	})
