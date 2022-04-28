4d28f00a6a5e35d664fadf76fb1f82f8dd07ebf8
Shanti Betts <Shanti.Betts@Gmail.com>
2022-04-27 18:04:44 -0700

    EOD Wednesday
    I got my projects section to pull data from google sheets! It's appending data, I just need to make sure that 1) I have all the icons for the skills I'm pulling and 2) the formatting is correct.

--- main.js
+++ main.js
@@ -37,7 +37,6 @@ $.ajax({
 			url: response.properties.forecastHourly
 		})
 			.then((response) => {
-				console.log(response)
 				$('.weather').append(
 					`<p>${response.properties.periods[0].shortForecast}, ${response.properties.periods[0].temperature}ºF</p>`
 				)
@@ -49,3 +48,40 @@ $.ajax({
 	.catch(() => {
 		console.log('error')
 	})
+
+const sheetUrl =
+	'https://docs.google.com/spreadsheets/d/1AVu1C-kfLCle-jHfPbu95fF1y2q9xJ6gVxpehcaWMRI/edit?usp=sharing'
+const sheetId = '1AVu1C-kfLCle-jHfPbu95fF1y2q9xJ6gVxpehcaWMRI'
+
+const key = 'AIzaSyBqiBqr5sQwZfIk_4qnWyFOp1TGBsMpUUs'
+
+const sheetsApiUrl = 'https://sheets.googleapis.com/v4/spreadsheets/'
+
+const rangeStart = 'a2'
+const rangeEnd = 'f5'
+let range = `/values/${rangeStart}:${rangeEnd}`
+
+let url = `${sheetsApiUrl}${sheetId}${range}?key=${key}`
+console.log(url)
+$.ajax({ url: url, key: key }).then((data) => {
+	console.log(data)
+	const projects = data.values
+	projects.forEach((project, i) => {
+		let tagsCSV = project[4]
+		let tags = tagsCSV.split(',')
+		$('#project1')
+			// Append Image source and alt text
+			.append(`<img src="${project[2]}" alt="${project[3]}"/>`)
+			// Append Project title and link
+			.append(`<a href="${project[5]}"><h4>${project[0]}</h4></a>`)
+			// Append project descritpion
+			.append(`<p>${project[1]}</p>`)
+			.append('<ul class="tags"></ul>')
+		// Append project tags
+		tags.forEach((tag) => {
+			$('#project1 ul').append(`<li>
+			<img src="assets/skillsIcons/${tag}.svg" alt="${tag} icon" />
+		</li>`)
+		})
+	})
+})
