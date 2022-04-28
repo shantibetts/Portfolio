6a99d24610879c095a485e777c8cb128b4d24145
Shanti Betts <Shanti.Betts@Gmail.com>
2022-04-28 12:57:58 -0700

    Sheets API Call working
    I had some issues with massaging strings into no-spaces and all-caps for IDs, but everything seems to be working now. I'm really happy with how the layout is working. Right now, when the data is fetched and creates HTML, the website scrolls down, which I would like it not to. Not sure where to start with this issue.

--- main.js
+++ main.js
@@ -53,38 +53,49 @@ const sheetUrl =
 	'https://docs.google.com/spreadsheets/d/1AVu1C-kfLCle-jHfPbu95fF1y2q9xJ6gVxpehcaWMRI/edit?usp=sharing'
 const sheetId = '1AVu1C-kfLCle-jHfPbu95fF1y2q9xJ6gVxpehcaWMRI'
 
-const key = 'AIzaSyBqiBqr5sQwZfIk_4qnWyFOp1TGBsMpUUs'
+const key = 'hidden'
 
 const sheetsApiUrl = 'https://sheets.googleapis.com/v4/spreadsheets/'
 
-const rangeStart = 'a2'
-const rangeEnd = 'f5'
+const rangeStart = 'a1'
+const rangeEnd = 'f8'
 let range = `/values/${rangeStart}:${rangeEnd}`
-console.log(url)
 $.ajax({
 	url: `${sheetsApiUrl}${sheetId}${range}?key=${key}`,
 	key: key
 }).then((data) => {
-	const projects = data.values
+	console.log(data)
+	const skillsCSV = data.values[1][0]
+	let skills = skillsCSV.split(',')
+	skills.forEach((skill) => {
+		$('#skills ul').append(
+			`<li><img src="assets/skillsIcons/${skill}.svg" alt="${skill} icon" /></li>`
+		)
+	})
+	const projects = data.values.slice(4)
 	projects.forEach((project) => {
 		let tagsCSV = project[4]
 		let tags = tagsCSV.split(',')
-		$('#project1')
+		let articleName = project[0].replace(/\s/g, '').toLowerCase()
+		let $article = $(`<article id="${articleName}" class="project"></article>`)
+		$article
 			// Append Image source and alt text
 			.append(
-				`<img class="projectImg" src="${project[2]}" alt="${project[3]}"/>`
+				`<img class="projectImg" src="${project[2]}" alt="${project[3]}" />`
 			)
 			// Append Project title and link
-			.append(`<a href="${project[5]}"><h4>${project[0]}</h4></a>`)
+			.append(
+				`<a href="${project[5]}" target="_blank"><h4>${project[0]}</h4></a>`
+			)
 			// Append project descritpion
 			.append(`<p>${project[1]}</p>`)
-			.append('<ul class="tags"></ul>')
-		// Append project tags
+		let $ul = $('<ul class="tags"></ul>')
 		tags.forEach((tag) => {
-			$('#project1 ul').append(`<li>
-			<img class="projectSkillsIcon"
-			src="assets/skillsIcons/${tag}.svg" alt="${tag} icon" />
-		</li>`)
+			let $li = $(`<li><img class="projectSkillsIcon"
+				src="assets/skillsIcons/${tag}.svg" alt="${tag} icon" /></li>`)
+			$ul.append($li)
 		})
+		$article.append($ul)
+		$('.portfolioContent').append($article)
 	})
 })
